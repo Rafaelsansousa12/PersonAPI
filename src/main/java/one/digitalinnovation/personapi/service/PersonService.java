464b37e35296b548ce81022e140168e5a9bf199a
Rafael Sousa <rafaelsansousa@hotmail.com>
2021-10-10 13:33:54 -0300

    Developed list all method

--- src/main/java/one/digitalinnovation/personapi/service/PersonService.java
+++ src/main/java/one/digitalinnovation/personapi/service/PersonService.java
@@ -9,6 +9,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 public class PersonService {
 
@@ -29,5 +32,10 @@ public class PersonService {
                 .builder()
                 .message("Created Person with ID " + savedPerson.getId())
                 .build();
+    }
+
+    public List<PersonDTO> listAll() {
+        List<Person> persons = personRepository.findAll();
+        return persons.stream().map(personMapper::toDTO).collect(Collectors.toList());
     }
 }
